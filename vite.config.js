cf9ed1d828d4f26181e63570e379e2bc372bc19f
kumiori1 <itsorinahum@gmail.com>
2025-09-18 07:24:40 +0100

    Fix __dirname using process.cwd()

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-
-// https://vite.dev/config/
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(process.cwd(), './src')
     }
   }
 })
