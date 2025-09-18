3924495ad5c641a80394e1fb4a9cccaed2ecd1d9
kumiori1 <itsorinahum@gmail.com>
2025-09-18 07:23:56 +0100

    Fix path module imports in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 @@ .. @@
  import { defineConfig } from 'vite'
  import react from '@vitejs/plugin-react'
- import path from 'path'
+import * as path from 'path'
 +import { fileURLToPath } from 'url'
 +import { dirname } from 'path'
 +
 +const __filename = fileURLToPath(import.meta.url)
-+const __dirname = dirname(__filename)
  
  // https://vite.dev/config/
- export default defineConfig({
+const __dirname = path.dirname(__filename)
  }
  )
